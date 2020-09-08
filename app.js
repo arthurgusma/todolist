5a10d784ab2056ae9aafbd11ebbcad333db788f8
Arthur Gusmao <arthur.cabj@hotmail.com>
2020-09-08 14:07:25 -0300

    Added EJS to the project and created a template to change the current day.

--- app.js
+++ app.js
@@ -2,9 +2,19 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const app = express()
 
+app.set("view engine", "ejs")
+
+
 app.get("/", (req, res) => {
-  res.send("Hello")
+  var today = new Date()
+  var currentDay = today.getDay()
+  var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+  res.render("list", {kindOfDay: day[currentDay]})
 })
+
+
+
 
 app.listen(3000, () => {
   console.log("server running.")
