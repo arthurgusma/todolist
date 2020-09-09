79974f959029645167b78bbb22cba29f70b3341a
Arthur Gusmao <arthur.cabj@hotmail.com>
2020-09-09 16:08:24 -0300

    Update: new way of adding new to do items.

--- app.js
+++ app.js
@@ -2,24 +2,31 @@ const express = require("express")
 const bodyParser = require("body-parser")
 const app = express()
 
+let items = []
+
+app.use(bodyParser.urlencoded({extended: true}))
 app.set("view engine", "ejs")
 
-
 app.get("/", (req, res) => {
-  var today = new Date()
+  const today = new Date()
 
-  var options = {
+  const options = {
     weekday: "long",
     day: "numeric",
     month: "long"
   }
-  var day = today.toLocaleDateString("en-US", options)
 
-  res.render("list", {kindOfDay: day})
+  const day = today.toLocaleDateString("en-US", options)
+
+  res.render("list", {kindOfDay: day, newListItems: items})
 
 })
 
-
+app.post("/", (req, res) => {
+  item = req.body.newItem
+  items.push(item)
+  res.redirect("/")
+})
 
 app.listen(3000, () => {
   console.log("server running.")
