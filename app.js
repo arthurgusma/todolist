856d9668498ffd886cf4c6a8f70bb9a6ee2a674b
Arthur Gusmao <arthur.cabj@hotmail.com>
2020-09-29 16:38:55 -0300

    Update: mongoose included to project.

--- app.js
+++ app.js
@@ -1,20 +1,51 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
+const mongoose = require('mongoose');
 
 const app = express();
 
-const items = [];
-const workItems = [];
+mongoose.connect('mongodb://localhost:27017/todolistDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 app.set("view engine", "ejs");
-
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
+
+const itemSchema = {
+  name: String
+};
+const Item = mongoose.model('Item', itemSchema);
+
+const item1 = new Item({
+  name: 'Breakfast'
+});
+const item2 = new Item({
+  name: 'lunch'
+});
+
+
+const defaultItems = [item1, item2]
+
+
+Item.insertMany(defaultItems, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Sucessfuly added.');
+  }
+});
 
 app.get("/", (req, res) => {
   const day = date.getDate();
-  res.render("list", {listTitle: day, newListItems: items});
+  res.render("list", {
+    listTitle: day,
+    newListItems: defaultItems
+  });
 });
 
 app.post("/", (req, res) => {
@@ -22,7 +53,7 @@ app.post("/", (req, res) => {
   console.log(req.body.list);
 
   if (req.body.list === "Work") {
-    workItems.push(item)
+    workItems.push()
     res.redirect("/work")
   } else {
     items.push(item)
@@ -31,7 +62,10 @@ app.post("/", (req, res) => {
 });
 
 app.get("/work", (req, res) => {
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
+  res.render("list", {
+    listTitle: "Work List",
+    newListItems: workItems
+  });
 });
 
 app.listen(3000, () => {
